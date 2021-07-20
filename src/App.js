d3f7b2fdb5f07a1675c1b7abb731f12cd7ca7409
Eric Shin <ericshin@Erics-MacBook-Pro.local>
2021-07-19 21:26:38 -0400

    first conmmit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React,{useState} from 'react'
 function App() {
+  const [ko,setko] = useState('')
+  const [en,setEn] = useState('')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="header">
+          <div className="container">
+            <h1 className="ko">{ko}</h1>
+            <h1 className="en">{en}</h1>
+          </div>
+        </header>
+        <div className="output">
+          <input onChange={(e)=>{setko(e.target.value)}} type="text" placeholder='글작성' />
+          <input type="text" placeholder="en" onChange={(e)=>{setEn(e.target.value)}}/>
+         </div>
     </div>
   );
 }
